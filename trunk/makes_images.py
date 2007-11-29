b33fe1a0fd34ac61f5f2dfefd43b105a8d283731
scottakirkwood <scottakirkwood@3eed6eb2-e13e-0410-8fff-15c9f7720fb5>
2007-11-29 00:39:21 +0000

    Latest danny settings

--- trunk/makes_images.py
+++ trunk/makes_images.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 
-for num in range(10):
-  for pos in range(8):
-    os.system('cp num-up-%d.gif num-up-%d-%d.gif' % (num, pos, num))
-    os.system('cp num-down-%d.gif num-down-%d-%d.gif' % (num, pos, num))
+for pos in range(8):
+    for num in range(10):
+        os.system('cp num-up-%d.gif num-up-%d-%d.gif' % (num, pos, num))
+        os.system('cp num-down-%d.gif num-down-%d-%d.gif' % (num, pos, num))
